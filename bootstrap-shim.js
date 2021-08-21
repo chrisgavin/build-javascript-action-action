397ba9b0dc79e52bc66dee3b9e5ff17aeb90fb4f
Chris Gavin <chris@chrisgavin.me>
2021-08-21 19:01:42 +0100

    Make some bootstrap shim improvements.

--- bootstrap-shim.js
+++ bootstrap-shim.js
@@ -7,13 +7,14 @@ const path = require("path");
 const target = path.join(__dirname, "build", "index.js");
 try {
 	if (!fs.existsSync(target)) {
-		console.warn("This is a development build! For greater reliability and faster startup time, please use a tagged release.");
+		console.log("::group::This is a development build! For greater reliability and faster startup time, please use a tagged release.");
 		childProcess.execFileSync("npm", ["run", "bootstrap"], {stdio: "inherit", cwd: __dirname});
+		console.log("::endgroup::");
 	}
 	childProcess.execFileSync(process.execPath, [target], {stdio: "inherit"});
 }
 catch (error) {
-	if (error.status === undefined) {
+	if (!error.status) {
 		throw error;
 	}
 	process.exitCode = error.status;
